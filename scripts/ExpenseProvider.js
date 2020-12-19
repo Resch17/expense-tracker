db230146c0c565d72020a11af07a4f9657e0221b
Aaron Resch <aaronmresch@gmail.com>
2020-12-18 20:12:17 -0600

    added expense provider

---
+++ scripts/ExpenseProvider.js
@@ -0,0 +1,38 @@
+let expenses = [];
+const expensesApi = 'http:localhost:8088/expenses';
+const eventHub = document.querySelector('.container');
+
+const dispatchStateChangeEvent = () => {
+  const customEvent = new CustomEvent('expensesStateChanged');
+  eventHub.dispatchEvent(customEvent);
+};
+
+export const getExpenses = () => {
+  return fetch(expensesApi)
+    .then((res) => res.json())
+    .then((parsedExpenses) => {
+      expenses = parsedExpenses;
+    });
+};
+
+export const useExpenses = () => expenses.slice();
+
+export const saveExpense = (expenseObject) => {
+  return fetch(expensesApi, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(expenseObject),
+  })
+    .then(getExpenses)
+    .then(dispatchStateChangeEvent);
+};
+
+export const deleteExpense = (expenseId) => {
+  return fetch(`${expensesApi}/${expenseId}`, {
+    method: 'DELETE',
+  })
+    .then(getExpenses)
+    .then(dispatchStateChangeEvent);
+};
