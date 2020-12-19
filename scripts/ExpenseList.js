faf0ee085fac859b2534f47022402307637fcb29
Aaron Resch <aaronmresch@gmail.com>
2020-12-18 23:02:58 -0600

    added total

--- scripts/ExpenseList.js
+++ scripts/ExpenseList.js
@@ -3,6 +3,7 @@ import { ExpenseRow } from './ExpenseRow.js';
 
 const eventHub = document.querySelector('.container');
 const listTarget = document.querySelector('.expense-target');
+const totalTarget = document.querySelector('#totalTarget')
 
 let expenses = [];
 
@@ -12,6 +13,8 @@ const expenseList = () => {
     listTarget.innerHTML = expenses
       .map((expense) => ExpenseRow(expense))
       .join('');
+    let expenseTotal = expenses.map((e) => parseFloat(e.amount)).reduce((a, b) => a + b, 0);
+    totalTarget.innerHTML = `$${expenseTotal}`;
   });
 };
 
@@ -23,3 +26,6 @@ eventHub.addEventListener('deleteExpenseClicked', (evt) => {
   const idToDelete = evt.detail.expenseToDelete;
   deleteExpense(idToDelete);
 });
+
+let expenseTotal = expenses.map((e) => e.amount).reduce((a, b) => a + b, 0);
+console.log(expenseTotal);
